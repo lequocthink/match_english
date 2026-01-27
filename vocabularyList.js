f7da5bc135ed2d1db92bceecbc80df1aa5e190d3
lequocthinh <lqt542001@gmail.com>
2026-01-27 13:20:42 +0700

    [2025-01-27 13:20] Add new vocabulary

--- vocabularyList.js
+++ vocabularyList.js
@@ -9,5 +9,12 @@ const vocabularyList = [
     { index: "3", english: "welcome", mean: "chào mừng", pronunciation: "UK: /ˈwelkəm/ | US: /ˈwelkəm/" },
     { index: "4", english: "please", mean: "vui lòng, làm ơn", pronunciation: "UK: /pliːz/ | US: /pliːz/" },
     { index: "5", english: "or", mean: "hoặc", pronunciation: "UK: /ɔː/ | US: /ɔːr/" },
+    { index: "6", english: "sugar", mean: "đường", pronunciation: "UK: /ˈʃʊɡə/ | US: /ˈʃʊɡər/" },
+    { index: "7", english: "milk", mean: "sữa", pronunciation: "UK: /mɪlk/ | US: /mɪlk/" },
+    { index: "8", english: "with", mean: "với", pronunciation: "UK: /wɪð/ | US: /wɪð/" },
+    { index: "9", english: "water", mean: "nước", pronunciation: "UK: /ˈwɔːtə/ | US: /ˈwɔːtər/" },
+    { index: "10", english: "goodbye", mean: "tạm biệt", pronunciation: "UK: /ˌɡʊdˈbaɪ/ | US: /ˌɡʊdˈbaɪ/" },
+    { index: "11", english: "and", mean: "và", pronunciation: "UK: /ænd/ | US: /ænd/" },
+    { index: "12", english: "thank you", mean: "cảm ơn", pronunciation: "UK: /ˈθæŋk juː/ | US: /ˈθæŋk juː/" },
 
 ];
