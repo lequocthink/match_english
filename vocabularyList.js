809d1fd74328c0f919c8d8ed1b919c17f75602f6
lequocthinh <lqt542001@gmail.com>
2026-08-02 18:59:36 +0700

    [02/08/2026 19:00] Add new vocabulary

--- vocabularyList.js
+++ vocabularyList.js
@@ -2,19 +2,45 @@
  * 1) DỮ LIỆU TỪ VỰNG (ví dụ)
  * Thay/ví dụ bằng 1000 từ của bạn.
  *******************************/
+// noun
+// pronoun
+// verb
+// adjective
+// adverb
+// preposition
+// conjunction
+// interjection
 const vocabularyList = [
-    //    // { index: "", english: "", mean: "", pronunciation: "" },
-    { index: "1", english: "tea", mean: "trà", pronunciation: "UK: /tiː/ | US: /tiː/" },
-    { index: "2", english: "coffee", mean: "cà phê", pronunciation: "UK: /ˈkɒfi/ | US: /ˈkɔːfi/" },
-    { index: "3", english: "welcome", mean: "chào mừng", pronunciation: "UK: /ˈwelkəm/ | US: /ˈwelkəm/" },
-    { index: "4", english: "please", mean: "vui lòng, làm ơn", pronunciation: "UK: /pliːz/ | US: /pliːz/" },
-    { index: "5", english: "or", mean: "hoặc", pronunciation: "UK: /ɔː/ | US: /ɔːr/" },
-    { index: "6", english: "sugar", mean: "đường", pronunciation: "UK: /ˈʃʊɡə/ | US: /ˈʃʊɡər/" },
-    { index: "7", english: "milk", mean: "sữa", pronunciation: "UK: /mɪlk/ | US: /mɪlk/" },
-    { index: "8", english: "with", mean: "với", pronunciation: "UK: /wɪð/ | US: /wɪð/" },
-    { index: "9", english: "water", mean: "nước", pronunciation: "UK: /ˈwɔːtə/ | US: /ˈwɔːtər/" },
-    { index: "10", english: "goodbye", mean: "tạm biệt", pronunciation: "UK: /ˌɡʊdˈbaɪ/ | US: /ˌɡʊdˈbaɪ/" },
-    { index: "11", english: "and", mean: "và", pronunciation: "UK: /ænd/ | US: /ænd/" },
-    { index: "12", english: "thank you", mean: "cảm ơn", pronunciation: "UK: /ˈθæŋk juː/ | US: /ˈθæŋk juː/" },
+    //    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "" },
+    { index: "1", english: "tea", mean: "trà", pronunciation: "UK: /tiː/ | US: /tiː/", partOfSpeech: "noun" },
+    { index: "2", english: "coffee", mean: "cà phê", pronunciation: "UK: /ˈkɒfi/ | US: /ˈkɔːfi/", partOfSpeech: "noun" },
+    { index: "3", english: "welcome", mean: "chào mừng", pronunciation: "UK: /ˈwelkəm/ | US: /ˈwelkəm/", partOfSpeech: "verb, adjective, interjection" },
+    { index: "4", english: "please", mean: "vui lòng, làm ơn", pronunciation: "UK: /pliːz/ | US: /pliːz/", partOfSpeech: "interjection, verb" },
+    { index: "5", english: "or", mean: "hoặc", pronunciation: "UK: /ɔː/ | US: /ɔːr/", partOfSpeech: "conjunction" },
+    { index: "6", english: "sugar", mean: "đường", pronunciation: "UK: /ˈʃʊɡə/ | US: /ˈʃʊɡər/", partOfSpeech: "noun" },
+    { index: "7", english: "milk", mean: "sữa", pronunciation: "UK: /mɪlk/ | US: /mɪlk/", partOfSpeech: "noun" },
+    { index: "8", english: "with", mean: "với", pronunciation: "UK: /wɪð/ | US: /wɪð/", partOfSpeech: "preposition" },
+    { index: "9", english: "water", mean: "nước", pronunciation: "UK: /ˈwɔːtə/ | US: /ˈwɔːtər/", partOfSpeech: "noun" },
+    { index: "10", english: "goodbye", mean: "tạm biệt", pronunciation: "UK: /ˌɡʊdˈbaɪ/ | US: /ˌɡʊdˈbaɪ/", partOfSpeech: "interjection, noun" },
+    { index: "11", english: "and", mean: "và", pronunciation: "UK: /ænd/ | US: /ænd/", partOfSpeech: "conjunction" },
+    { index: "12", english: "thank you", mean: "cảm ơn", pronunciation: "UK: /ˈθæŋk juː/ | US: /ˈθæŋk juː/", partOfSpeech: "interjection" },
+    { index: "13", english: "Ball", mean: "Quả bóng", pronunciation: "/bɔːl/", partOfSpeech: "Noun" },
+    { index: "14", english: "Bike", mean: "Xe đạp", pronunciation: "/baɪk/", partOfSpeech: "Noun" },
+    { index: "15", english: "Book", mean: "Sách", pronunciation: "/bʊk/", partOfSpeech: "Noun" },
+    { index: "16", english: "School", mean: "Trường học", pronunciation: "/skuːl/", partOfSpeech: "Noun" },
+    { index: "17", english: "Play", mean: "Chơi", pronunciation: "/pleɪ/", partOfSpeech: "Verb" },
+    // { index: "18", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "19", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "20", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
+    // { index: "", english: "", mean: "", pronunciation: "", partOfSpeech: "noun" },
 
 ];
